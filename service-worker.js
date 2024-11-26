96f146105004b0e6dd7f17f06ac457b835a7a52e
anichat21 <72806695+anichat21@users.noreply.github.com>
2024-11-26 12:38:39 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('stock-app-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js',
+                '/manifest.json',
+                '/images/icon-192.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
 
